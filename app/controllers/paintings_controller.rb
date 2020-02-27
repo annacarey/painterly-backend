afa03c0dfbe088b550c5ebcc43b906bdde3f78a6
Your Name <samanbatool08@gmail.com>
2020-02-27 18:01:47 -0500

    post paintings

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -5,4 +5,26 @@ def index
     render json: paintings
 end
 
+def create
+    painting = Painting.create(painting_params)
+    # byebug
+    render json: painting
 end
+
+
+private
+
+def painting_params
+  grid = params.require(:grid).map {|array| array}
+  title = params.require(:painting).permit(:user_id, :title)[:title]
+  user_id = params.require(:painting).permit(:user_id, :title)[:user_id]
+
+  {:grid => grid, :title => title, :user_id =>  user_id }
+end
+
+
+end
+
+# params.require(:rss).map do |p|
+#     p.permit(:rss, :type)
+#   end
