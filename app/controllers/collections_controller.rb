d6a8d8503140b5cbeee10a5674375786bd72830f
Your Name <samanbatool08@gmail.com>
2020-02-28 14:52:28 -0500

    collections routes

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,24 @@
 class CollectionsController < ApplicationController
+
+    def index
+        collections = Collection.all 
+        render json: collections
+    end
+
+
+    def create 
+        collection = Collection.create(collection_params)
+        render json: collection
+    end
+
+    def show
+        collection = Collection.find_by(params[:id])
+        render json: collection
+    end
+
+private
+    def collection_params
+        params.permit(:titile, :user_id)
+    end 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,13 @@ User.destroy_all
 u1 = User.create(username: "Anna", password: "123")
 u2 = User.create(username: "Saman", password: "123")
 
-c1 = Collection.create(title: "first book", user: u1)
-c2 = Collection.create(title: "second book", user: u2)
+c1 = Collection.create(title: "first collection", user: u1)
+c2 = Collection.create(title: "second collection", user: u2)
+c3 = Collection.create(title: "random collection", user: u1)
+c4 = Collection.create(title: "draft collection", user: u1)
+c5 = Collection.create(title: "lazy collection", user: u1)
+c6 = Collection.create(title: "work collection", user: u1)
+
 
 
 
