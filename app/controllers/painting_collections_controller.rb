06778bab2f1fd1a42e508d5d1ecd265261fd53b8
github username <github email address>
2020-03-02 17:45:52 -0500

    added route for paintingcollectiongs

--- app/controllers/painting_collections_controller.rb
+++ app/controllers/painting_collections_controller.rb
@@ -2,7 +2,12 @@ class PaintingCollectionsController < ApplicationController
     def create 
         painting_collection = PaintingCollection.create(painting_collection_params)
         render json: painting_collection, except: [:created_at, :updated_at]
-      end 
+    end 
+
+    def index
+        painting_collections = PaintingCollection.all
+        render json: painting_collections
+    end
     
     private 
     
