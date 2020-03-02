50da31ee7f55b92bf8ec808b543897a0361330ed
github username <github email address>
2020-03-02 15:11:11 -0500

    fixed painting validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     def create 
         user = User.new(user_params)
         if user.save
-        render json: user, except: [:created_at, :updated_at]
+            render json: user, except: [:created_at, :updated_at]
         else 
             render json: {errors: user.errors.full_messages}
         end
