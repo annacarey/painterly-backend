c4bc942d53e0ffcc6b2548f9cf2637e6f7cda8ed
github username <github email address>
2020-03-02 17:13:37 -0500

    added painting-collections

---
+++ app/controllers/painting_collections_controller.rb
@@ -0,0 +1,13 @@
+class PaintingCollectionsController < ApplicationController
+    def create 
+        painting_collection = PaintingCollection.create(painting_collection_params)
+        render json: painting_collection, except: [:created_at, :updated_at]
+      end 
+    
+    private 
+    
+    def painting_collection_params
+        params.require(:painting_collection).permit(:painting_id, :collection_id)
+    end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :collections, only: [:index, :create, :show]
 
+  resources :painting_collections, only: [:create]
+
 end
 
 
